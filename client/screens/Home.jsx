9b757a228ddcec466f670760770dad1019ac82d9
Savannah Manning <63322482+savhappy@users.noreply.github.com>
2022-03-20 11:55:05 -0400

    added basic nav & globalstyles

--- client/screens/Home.jsx
+++ client/screens/Home.jsx
@@ -1,10 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, Button, TouchableOpacity } from "react-native";
 import React from "react";
+import globalStyles from "../styles/globalStyles";
 
-const Home = () => {
+const Home = ({ navigation }) => {
+  const pressHandler = () => {
+    navigation.navigate("JoinGame");
+  };
+  const pressHandlerRoom = () => {
+    navigation.navigate("Room");
+  };
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>HOME SCREEN</Text>
+      <TouchableOpacity onPress={pressHandler}>
+        <Text style={globalStyles.titleText}>JOIN GAME (test btn)</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={pressHandlerRoom}>
+        <Text style={globalStyles.titleText}>NEW GAME (test btn)</Text>
+      </TouchableOpacity>
     </View>
   );
 };
---
+++ client/screens/JoinGame.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+import globalStyles from "../styles/globalStyles";
+
+const JoinGame = () => {
+  return (
+    <View style={globalStyles.container}>
+      <Text style={globalStyles.titleText}>JoinGame</Text>
+    </View>
+  );
+};
+
+export default JoinGame;
