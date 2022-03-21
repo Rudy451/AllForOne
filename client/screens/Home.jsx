abdb9500ed7d3312f6fe748c2469eb76528b17a3
Fatima Tapiya <fatima031598@gmail.com>
2022-03-21 15:26:02 -0600

    working on the global styles

--- client/screens/Home.jsx
+++ client/screens/Home.jsx
@@ -1,6 +1,8 @@
-import { View, Text, Button, TouchableOpacity } from 'react-native';
+import { View, Text, Button, TouchableOpacity, Image } from 'react-native';
 import React from 'react';
 import globalStyles from '../styles/globalStyles';
+import logo from '../assets/logo.png';
+// import RadialGradient from 'react-native-radial-gradient';
 
 const Home = ({ navigation }) => {
   const pressHandler = () => {
@@ -11,12 +13,22 @@ const Home = ({ navigation }) => {
   };
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>HOME SCREEN</Text>
-      <TouchableOpacity onPress={pressHandlerRoom}>
-        <Text style={globalStyles.titleText}>NEW GAME (test btn)</Text>
+      {/* <RadialGradient
+        style={{ width: 200, height: 200 }}
+        colors={['black', 'green', 'blue', 'red']}
+        stops={[0.1, 0.4, 0.3, 0.75]}
+        center={[100, 100]}
+        radius={200}
+      ></RadialGradient> */}
+      <Image source={logo} />
+      <TouchableOpacity
+        onPress={pressHandlerRoom}
+        style={globalStyles.lightBtn}
+      >
+        <Text style={globalStyles.buttonText}>NEW GAME</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={pressHandler}>
-        <Text style={globalStyles.titleText}>JOIN GAME (test btn)</Text>
+      <TouchableOpacity onPress={pressHandler} style={globalStyles.lightBtn}>
+        <Text style={globalStyles.buttonText}>JOIN GAME</Text>
       </TouchableOpacity>
     </View>
   );
--- client/styles/globalStyles.jsx
+++ client/styles/globalStyles.jsx
@@ -8,17 +8,72 @@ const globalStyles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  glowContainer: {},
-  lightContainer: {}, //lighter grey containers border radius on everything is 5
-  glowTitle: {},
-
-  titleText: {
+  lightContainer: {
+    backgroundColor: '#182624',
+    width: 304,
+    height: 140,
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+  },
+  darkContainer: {
+    backgroundColor: 'black',
+    width: 144,
+    height: 55,
+    borderWidth: 1,
+    borderRadius: 5,
+    alignSelf: 'center',
+    padding: 5,
+  },
+  glowTitle: {
+    //still working on this
+    color: 'white',
+    fontSize: 36,
+    fontFamily: 'AvenirNextCondensed-Medium',
+    textAlign: 'center',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: { width: -1, height: 1 },
+    textShadowRadius: 10,
+  },
+  titleTextBold: {
+    color: 'white',
+    fontSize: 36,
+    fontFamily: 'AvenirNextCondensed-Bold',
+  },
+  titleTextMedium: {
+    color: 'white',
+    fontSize: 36,
+    fontFamily: 'AvenirNextCondensed-Medium',
+    textAlign: 'center',
+  },
+  subText: {
+    color: 'white',
+    fontSize: 18,
+    fontFamily: 'Avenir Next Condensed',
+    padding: 10,
+    textAlign: 'center',
+  },
+  darkBtn: {
+    marginTop: 20,
+    borderRadius: 10,
+    padding: 10,
+    paddingHorizontal: 50,
+    elevation: 2,
+    backgroundColor: '#182724',
+  },
+  lightBtn: {
+    marginTop: 20,
+    borderRadius: 10,
+    padding: 10,
+    paddingHorizontal: 50,
+    elevation: 2,
+    backgroundColor: '#00E6B7',
+  },
+  buttonText: {
     color: 'white',
     fontSize: 24,
+    fontFamily: 'AvenirNextCondensed-Bold',
   },
-  subText: {},
-  darkBtn: {},
-  lightBtn: {},
 });
 
 export default globalStyles;
