0d4e303a3898227ca5c327b890df5b8b43835bc6
Fatima Tapiya <fatima031598@gmail.com>
2022-03-21 10:04:10 -0600

    deleted the drawer

--- client/screens/Main.jsx
+++ client/screens/Main.jsx
@@ -1,25 +1,39 @@
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import React, { useState } from 'react';
 import globalStyles from '../styles/globalStyles';
 import { Entypo } from '@expo/vector-icons';
+import RulesModal from '../modals/Rules';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 const Main = () => {
+  const [modalVisible, setModalVisible] = useState(false);
   console.log(windowWidth);
   console.log(windowHeight);
+
+  const openDrawer = () => {
+    console.log('clicked');
+    setModalVisible(!modalVisible);
+  };
   return (
     <View
       style={{ ...globalStyles.container, borderWidth: 2, borderColor: 'red' }}
     >
-      <View style={styles.header}>
+      <TouchableOpacity style={styles.header} onPress={openDrawer}>
         <Entypo name='menu' size={45} color='white' />
-      </View>
+      </TouchableOpacity>
+      <RulesModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+      ></RulesModal>
       <Text style={globalStyles.titleText}>Main(geo map)</Text>
-      <Text style={globalStyles.titleText}>
-        (still swipes back, must prevent this)
-      </Text>
     </View>
   );
 };
