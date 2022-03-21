239f12663201f3f9c83a28be2b6c160a2fc6277d
Fatima Tapiya <fatima031598@gmail.com>
2022-03-21 11:00:00 -0600

    working on global styling

--- client/styles/globalStyles.jsx
+++ client/styles/globalStyles.jsx
@@ -1,18 +1,24 @@
-import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
 
 const globalStyles = StyleSheet.create({
   container: {
-    backgroundColor: "#0b1313",
+    backgroundColor: '#0b1313',
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  glowContainer: {},
+  lightContainer: {}, //lighter grey containers border radius on everything is 5
+  glowTitle: {},
+
   titleText: {
-    color: "white",
+    color: 'white',
     fontSize: 24,
   },
   subText: {},
+  darkBtn: {},
+  lightBtn: {},
 });
 
 export default globalStyles;
