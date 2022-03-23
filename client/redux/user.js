458a2100dd0019d604f845d37b482280bedc87ef
Savannah Manning <63322482+savhappy@users.noreply.github.com>
2022-03-23 15:43:32 -0400

    redux toolkit plus cities folder

---
+++ client/redux/user.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState: {
+    type: "",
+  },
+  reducers: {
+    setTypeCaptain: (state) => {
+      state.type = "Captain";
+    },
+    setTypePlayer: (state) => {
+      state.type = "Captain";
+    },
+    setType: (state, action) => {
+      state.type = action.payload;
+    },
+  },
+});
+export const { setTypeCaptain, setTypePlayer } = userSlice.actions;
+export default userSlice.reducer;
