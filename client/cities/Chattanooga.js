7ed4cb37f15ebee651240131c2fdd579eb533b85
Savannah Manning <63322482+savhappy@users.noreply.github.com>
2022-03-23 15:43:32 -0400

    redux toolkit plus cities folder

---
+++ client/cities/Chattanooga.js
@@ -0,0 +1,81 @@
+const Chattanooga = [
+  {
+    id: 1,
+    location: "Game Start",
+    City: "Chattanooga",
+    state: "TN",
+    latitude: 35.05817,
+    longitude: -85.307296,
+    question:
+      "This is the start of your scavanger hunt. Go to the given location and press check-in when you believe you are there. If correct a question will be displayed on a similar pop-up as this.",
+  },
+  {
+    id: 2,
+    location: "Ice Cream Show",
+    City: "Chattanooga",
+    state: "TN",
+    latitude: 35.05552237416245,
+    longitude: -85.30774106324982,
+    question:
+      "What's the name of the dog that's always ready to give you a hi five?",
+  },
+
+  {
+    id: 3,
+    location: "Old Building Collapse",
+    City: "Chattanooga",
+    state: "TN",
+    latitude: 35.05490819312549,
+    longitude: -85.30943785558155,
+    question:
+      "What restaurant originally occupied this spot before it gave way to a crumbling roof?",
+  },
+  {
+    id: 4,
+    location: "River Street Deli",
+    City: "Chattanooga",
+    state: "TN",
+    latitude: 35.062182520255526,
+    longitude: -85.30772240419256,
+    question: "What state is the owner of River Street Deli from?",
+  },
+  {
+    id: 5,
+    location: "Clumpies",
+    City: "Chattanooga",
+    state: "TN",
+    latitude: 35.06245598491883,
+    longitude: -85.30768626201176,
+    question: "What kind of dance do you do in front of Clumpies?",
+  },
+  {
+    id: 6,
+    location: "Coolidge Park",
+    City: "Chattanooga",
+    state: "TN",
+    latitude: 35.061314786199006,
+    longitude: -85.30759823642703,
+    question: "How many creatures are permanently at Coolidge park?",
+  },
+  {
+    id: 7,
+    location: "Renaissance Park",
+    City: "Chattanooga",
+    state: "TN",
+    latitude: 35.060361641139934,
+    longitude: -85.31094593831801,
+    question:
+      " At Renaissance Park, what is the name of the artistic piece that sends you on a walk through wires and vines?",
+  },
+  {
+    id: 8,
+    location: "Hunter Art Museum",
+    City: "Chattanooga",
+    state: "TN",
+    latitude: 35.056077912031526,
+    longitude: -85.30664918880245,
+    question: "What is the equine statue made of?",
+  },
+];
+
+export default Chattanooga;
