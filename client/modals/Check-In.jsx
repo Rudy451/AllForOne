4986e6fb2ba09bba07665451ab732f12c8ec51e3
Savannah Manning <63322482+savhappy@users.noreply.github.com>
2022-03-24 10:52:59 -0400

    check in component done

--- client/modals/Check-In.jsx
+++ client/modals/Check-In.jsx
@@ -14,39 +14,38 @@ function CheckInModal({ setLocation }) {
     console.log("This is current location", location);
   };
   return (
-    <View style={styles.centeredView}>
-      <View style={styles.modalView}>
-        <View style={styles.textView}>
-          <Text style={[globalStyles.subText, { justifyContent: "flex-end" }]}>
-            This is the start of your scavanger hunt. Go to the given location
-            and press check-in when you believe you are there. If correct a
-            question will be displayed in this.
-          </Text>
-        </View>
-        <TouchableOpacity style={styles.button} onPress={onPress}>
-          <Text style={[globalStyles.titleTextBold, { textAlign: "center" }]}>
-            Check-In
-          </Text>
-        </TouchableOpacity>
-      </View>
+    // <View style={styles.centeredView}>
+    <View style={styles.modalView}>
+      {/* <View style={styles.textView}> */}
+      <Text style={[globalStyles.subText, { marginHorizontal: 20 }]}>
+        This is the start of your scavanger hunt. Go to the given location and
+        press check-in when you believe you are there. If correct a question
+        will be displayed in this.
+      </Text>
+      {/* </View> */}
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Text style={[globalStyles.titleTextBold, { textAlign: "center" }]}>
+          Check-In
+        </Text>
+      </TouchableOpacity>
     </View>
+    // </View>
   );
 }
 const styles = StyleSheet.create({
-  centeredView: {
-    justifyContent: "flex-end",
-    alignItems: "center",
-  },
+  centeredView: {},
   modalView: {
     backgroundColor: "#182624",
     width: "95%",
-
+    bottom: "2%",
+    position: "absolute",
+    alignItems: "center",
     borderRadius: 20,
-    marginBottom: "5%",
-    justifyContent: "flex-end",
+    paddingTop: 10,
   },
   textView: {
-    paddingHorizontal: 10,
+    margin: 20,
+    position: "relative",
   },
   button: {
     marginTop: 20,
@@ -55,24 +54,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#00E6B7",
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
-  },
+    width: "100%",
 
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "left",
-  },
-  modalSubText: {
-    marginBottom: 10,
-    fontSize: 15,
-    fontWeight: "normal",
-    textAlign: "left",
+    position: "relative",
   },
 });
 
