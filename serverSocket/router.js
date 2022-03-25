302293734eadee5b6b18a9f3bdc1c2fdef693d2c
Savannah Manning <63322482+savhappy@users.noreply.github.com>
2022-03-25 13:32:22 -0400

    Merge branch 'main' of https://github.com/savhappy/AllForOne into savannah

---
+++ serverSocket/router.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+// const controller = require('./controllers/controller')
+
+// router.get('/events', controller.getAllEvents)
+
+// router.get('/events/:id', controller.getById)
+
+// router.post('/events', controller.postEvent)
+
+// router.delete('/events/:id', controller.deleteEvent)
+
+module.exports = router;
