9918b43937de817141f1a2dec8711950bd433a61
Rudy451 <dane.johnson451@gmail.com>
2022-06-09 10:36:54 -0700

    Changed some backend stuff for aws deployment

--- gameplay_server/gameapp/gameapp/wsgi.py
+++ gameplay_server/gameapp/gameapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gameapp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gameapp.gameapp.settings')
 
 application = get_wsgi_application()
